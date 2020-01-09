891e43af5239037599e631ce28df2febaeaa05e5
melvin asare <melvinasareb@gmail.com>
2020-01-09 14:43:45 +0000

    Initial Commit

---
+++ NewsApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp2
+//
+//  Created by Melvin Asare  on 09/01/2020.
+//  Copyright © 2020 Melvin Asare . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
